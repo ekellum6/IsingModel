5f2759013320fedb7c39e230885d970df6058b7e
ABoresow <alex.boresow@gmail.com>
2025-04-10 22:54:58 -0400

    Rewrote 1,2,3-D models with numba jit compilation

---
+++ 2Dising.py
@@ -0,0 +1,127 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.special as sp
+from scipy import stats
+from numba import jit
+
+L = 100 #number of lattice points in each dimension
+size = L*L #total number of lattice points
+J = 1.0  #Coupling Coefficient
+N = 1000 #number of monte-carlo steps for data collection
+Neq = 1000 #number of MCS for equilibration
+Nt = 100 #number of temperatures sampled
+Temps = np.linspace(0.1,10,Nt)
+
+#Data Collection
+M_t = np.zeros(N) #magnetization
+E_t = np.zeros(N) #energy per spin/lattice point
+E2_t = np.zeros(N) #energy^2 per spin/lattice point
+s_T = []
+M_T = np.zeros(Nt)
+E_T = np.zeros(Nt)
+Cv_T = np.zeros(Nt)
+
+
+@jit
+def MCS(state,expvals,L=L,size=size,J=J):
+    randxy = np.random.randint(0,L,(size,2))
+    for n_in in range(size):
+        #choose a random lattice point
+        xpos = randxy[n_in,0]
+        xp = xpos+1
+        if xp == L:
+            xp = 0
+        xm = xpos-1
+        ypos = randxy[n_in,1]
+        yp = ypos+1
+        if yp == L:
+            yp = 0
+        ym = ypos-1
+        #calculate change in energy of prospective flip
+        dU = 2*J*state[xpos,ypos]*(state[xp,ypos] + state[xm,ypos] + state[xpos,yp] + state[xpos,ym])
+        #decide whether to flip
+        if dU <= 0:
+            state[xpos,ypos] *= -1
+        elif np.random.rand() < expvals[int(dU/4)]:
+            state[xpos,ypos] *= -1
+    return state
+
+#Iterate over Temps
+Tcounter = 0
+for T in Temps:
+    print(Tcounter)
+    #Precompute exponentials
+    expvals = [0.,np.exp(-4/T),np.exp(-8/T)]
+    #Begin in Random Initial State
+    state = np.random.choice([-1,1],(L,L))
+    state_i = state.copy()
+    #Begin Iteration of MCS (monte-carlo steps)
+    for n_out in range(N+Neq):
+        if n_out >= Neq:
+            #compute and store magnetization
+            M_t[n_out-Neq] = np.sum(state)/size
+            #compute and store energy per spin
+            E_t[n_out-Neq] = -J*np.sum(state*(np.roll(state,1,0)+np.roll(state,1,1)))/size
+            #compute and store E^2 per spin
+            E2_t[n_out-Neq] = E_t[n_out-Neq]**2
+        #For each MCS, attempt flipping one spin for every lattice point
+        MCS(state,expvals)
+    M_T[Tcounter] = stats.mode(np.abs(M_t),axis=None)[0]
+    E_T[Tcounter] = np.sum(E_t)/(N)
+    Cv_T[Tcounter] = ((np.sum(E2_t)/N)-(E_T[Tcounter]**2))/(T**2)
+    state_f = state.copy()
+    s_T.append((state_i,state_f))
+    Tcounter += 1
+
+
+#Analytical Solution:
+def analyticalE(T):
+    k = (2*np.sinh(2*J/T))/(np.cosh(2*J/T))**2
+    K = sp.ellipk(k**2)
+    return (-J/np.tanh(2*J/T))*(1+(2/np.pi)*(-1+2*(np.tanh(2*J/T))**2)*K)
+exactE_T = np.array([analyticalE(T) for T in Temps])
+#Plot <E/N>(T)
+plt.plot(Temps,E_T, label='Numerical Result')
+plt.plot(Temps, exactE_T, label='Analytical Result')
+plt.title("Analytical vs. Numerical Solution of the 2-D Ising Model")
+#plt.xscale('log')
+plt.xlabel("Temperature")
+plt.ylabel("<E/N>")
+plt.legend()
+#plt.savefig(f'Figures/2D/2D-EnergyComparison.jpg')
+plt.show()
+
+#Plot Cv(T)/N
+plt.plot(Temps,Cv_T, label='Numerical Result')
+#plt.plot(Temps, exactCv_T, label='Analytical Result')
+plt.title("Cv(T) for 2-D Ising Model")
+#plt.xscale('log')
+plt.xlabel("Temperature")
+plt.ylabel("Cv/N")
+#plt.legend()
+#plt.savefig(f'Figures/1D/1D-EnergyComparison.jpg')
+plt.show()
+
+#Plot <M>(T)
+plt.plot(Temps,M_T)
+#plt.xscale('log')
+plt.show()
+
+#Plot Spin Configurations
+for i_T in range(Nt):
+    print(f'Temperature: {Temps[i_T]}')
+    f,ax = plt.subplots(1,2)
+    ax[0].imshow(s_T[i_T][0],cmap='gray')
+    ax[0].set_xticks([]);
+    ax[0].set_yticks([]);
+    ax[0].xaxis.set_label_position('bottom')
+    ax[0].set_xlabel('Initial State',fontsize=16)
+    ax[1].imshow(s_T[i_T][1],cmap='gray')
+    ax[1].set_xticks([]);
+    ax[1].set_yticks([]);   
+    ax[1].xaxis.set_label_position('bottom')
+    ax[1].set_xlabel('Final State',fontsize=16)
+    f.suptitle(f'T = {Temps[i_T]:.4f}',y=.85,fontsize=16)
+    plt.tight_layout()
+    #plt.savefig(f'Figures/2D/2D-spinconfigplots/2Dconfig{i_T+1}.jpg')
+    plt.show()
